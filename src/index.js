0a68626e463d7c602adde2aa58e466117e1cf48b
jacob <jacobharv00@gmail.com>
2022-09-03 16:45:43 -0600

    feat: setup auth for signin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require( 'express' )
 const mongoose = require( 'mongoose' )
 const bodyParser = require( 'body-parser' )
 const mongoUri = require( '../config.js' )
+const requireAuth = require( './middlewares/requireAuth' )
 
 const authRoutes = require( './routes/authRoutes' )
 
@@ -21,8 +22,8 @@ mongoose.connection.on( 'error', ( err ) => {
   console.error( 'Error connecting to mongo', err )
 } )
 
-app.get( '/', ( request, response ) => {
-  response.send( 'Hi there!' )
+app.get( '/', requireAuth, ( req, res ) => {
+  res.send( `Your email: ${req.user.email}` )
 } )
 
 app.listen( 3000, () => {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,21 @@
 const express = require( 'express' )
 const mongoose = require( 'mongoose' )
+const jwt = require( 'jsonwebtoken' )
 
 const router = express.Router()
 const User = mongoose.model( 'User' )
 
 router.post( '/signup', async ( req, res ) => {
   const { email, password } = req.body
-
-  const user = new User( { email, password } )
-  await user.save()
-
-  res.send( 'You made a post request' )
+  try {
+    const user = new User( { email, password } )
+    await user.save()
+    const token = jwt.sign( { userId: user._id }, 'MY_SECRET_KEY' )
+    res.send( { token } )
+  } catch ( error ) {
+    // TODO: Change to user readable error message
+    return res.status( 422 ).send( error.message )
+  }
 } )
 
 module.exports = router
