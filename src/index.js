be93065f0a6507cda9d28403379de15bb410af85
jacob <jacobharv00@gmail.com>
2022-09-05 10:59:55 -0600

    feat: add get and post routes for tracks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require( './models/User' )
+require( './models/Track' )
 const express = require( 'express' )
 const mongoose = require( 'mongoose' )
 const bodyParser = require( 'body-parser' )
@@ -6,11 +7,13 @@ const mongoUri = require( '../config.js' )
 const requireAuth = require( './middlewares/requireAuth' )
 
 const authRoutes = require( './routes/authRoutes' )
+const trackRoutes = require( './routes/trackRoutes' )
 
 const app = express()
 
 app.use( bodyParser.json() )
 app.use( authRoutes )
+app.use( trackRoutes )
 
 mongoose.connect( mongoUri )
 
